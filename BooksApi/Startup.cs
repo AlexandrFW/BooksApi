21856d9c0031fad1cb1b23f545f730ed5735812a
Alexandr Kutiev <antizboy1985@gmail.com>
2020-03-12 19:25:13 +0300

    Adding CRUD logic with database connection

--- BooksApi/Startup.cs
+++ BooksApi/Startup.cs
@@ -1,4 +1,5 @@
 using BooksApi.Models;
+using BooksApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,8 @@ namespace BooksApi
             services.Configure<BookstoreDatabaseSettings>(Configuration.GetSection(nameof(BookstoreDatabaseSettings)));
             services.AddSingleton<IBookstoreDatabaseSettings>(sp =>
                      sp.GetRequiredService<IOptions<BookstoreDatabaseSettings>>().Value);
+
+            services.AddSingleton<BookService>();
 
             services.AddControllers();
         }
