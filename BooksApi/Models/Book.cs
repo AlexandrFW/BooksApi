ae6d9d5ee0069b835192ec8face8a261f264f6e7
Alexandr Kutiev <antizboy1985@gmail.com>
2020-03-12 18:34:48 +0300

    Add project files.

---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string BookName { get; set; }
+    }
+}
